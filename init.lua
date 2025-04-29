c3fbd723dd4180352a07618c3f95d6a0d2979532
Broken Sunny <763184085@qq.com>
2025-04-29 21:26:19 +0800

    Update

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local K = require("plugin-keymap")
 K.setup()
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
--- lua/custom/plugins/clone-word/init.lua
+++ lua/custom/plugins/clone-word/init.lua
@@ -1,49 +1,53 @@
 local M = {}
 
-function M.copy_line_word(dir, count)
-  local mode = vim.api.nvim_get_mode().mode
-  local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+local function should_clone_down(cursor_row, cursor_col)
   local buf_line_count = vim.api.nvim_buf_line_count(0)
+  if buf_line_count == cursor_row then
+    return false
+  end
 
-  local lines
+  local line = vim.api.nvim_buf_get_lines(0, cursor_row, cursor_row + 1, false)[1]
+  if #line <= cursor_col + 1 then
+    return false
+  end
 
-  if dir == "up" then
-    if cursor_row == 1 then
-      return
-    end
+  return true
+end
 
-    -- if mode == "i" then
-    --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
-    -- end
+local function should_clone_up(cursor_row)
+  if cursor_row == 1 then
+    return false
+  end
+  return true
+end
 
+local function clone_up(count, mode, cursor_row, cursor_col)
+  if not should_clone_up(cursor_row) then return end
+
+  if mode == "n" then
     vim.api.nvim_feedkeys("k", "n", true)
-
     if cursor_col ~= 0 then
       vim.api.nvim_feedkeys("l", "n", true)
     end
-
     vim.api.nvim_feedkeys("v" .. count .. "e" .. '"ayj', "n", true)
     vim.api.nvim_feedkeys("i", "n", true)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-R>a", true, true, true), "n", true)
-
-    if mode == "n" then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+  elseif mode == "i" then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+    vim.api.nvim_feedkeys("k", "n", true)
+    if cursor_col ~= 0 then
+      vim.api.nvim_feedkeys("l", "n", true)
     end
-  elseif dir == "down" then
-    if buf_line_count == cursor_row then
-      return
-    end
+    vim.api.nvim_feedkeys("v" .. count .. "e" .. '"ayj', "n", true)
+    vim.api.nvim_feedkeys("i", "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-R>a", true, true, true), "n", true)
+  end
+end
 
-    lines = vim.api.nvim_buf_get_lines(0, cursor_row, cursor_row + 1, false)
-
-    if #lines[1] <= cursor_col + 1 then
-      return
-    end
-
-    if mode == "i" then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
-    end
-
+local function clone_down(count, mode, cursor_row, cursor_col)
+  if not should_clone_down(cursor_row, cursor_col) then return end
+  if mode == "n" then
     vim.api.nvim_feedkeys("j", "n", true)
     if cursor_col ~= 0 then
       vim.api.nvim_feedkeys("l", "n", true)
@@ -51,10 +55,29 @@ function M.copy_line_word(dir, count)
     vim.api.nvim_feedkeys("v" .. count .. "e" .. '"ayk', "n", true)
     vim.api.nvim_feedkeys("i", "n", true)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-R>a", true, true, true), "n", true)
-
-    if mode == "n" then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+  elseif mode == "i" then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+    vim.api.nvim_feedkeys("j", "n", true)
+    if cursor_col ~= 0 then
+      vim.api.nvim_feedkeys("l", "n", true)
     end
+    vim.api.nvim_feedkeys("v" .. count .. "e" .. '"ayk', "n", true)
+    vim.api.nvim_feedkeys("i", "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-R>a", true, true, true), "n", true)
+  end
+end
+
+--- @param dir "up"|"down"
+--- @param count integer
+function M.copy_line_word(dir, count)
+  local mode = vim.api.nvim_get_mode().mode
+  local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+
+  if dir == "up" then
+    clone_up(count, mode, cursor_row, cursor_col)
+  elseif dir == "down" then
+    clone_down(count, mode, cursor_row, cursor_col)
   end
 end
 
