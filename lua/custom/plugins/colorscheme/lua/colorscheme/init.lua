a4ef9c7a4229956d75958d3609f7711f324fc66f
luoqing <763184085@qq.com>
2025-08-22 08:02:38 +0800

    update

--- lua/custom/plugins/colorscheme/lua/colorscheme/init.lua
+++ lua/custom/plugins/colorscheme/lua/colorscheme/init.lua
@@ -3,27 +3,93 @@ local M = {}
 local function get_colors()
   local color_groups = {
     dark = {
-      black0 = "#000000",
-      black1 = "#0d0d0d",
-      black2 = "#191919",
-      black3 = "#262626",
-      black4 = "#333333",
-      black5 = "#404040",
-      black6 = "#4d4d4d",
-      black7 = "#595959",
-      black8 = "#666666",
-      black9 = "#737373",
-      black10 = "#808080",
-      black11 = "#8C8C8C",
-      black12 = "#999999",
-      black13 = "#A6A6A6",
-      black14 = "#B3B3B3",
-      black15 = "#BFBFBF",
-      black16 = "#CCCCCC",
-      black17 = "#D9D9D9",
-      black18 = "#E6E6E6",
-      black19 = "#F2F2F2",
-      black20 = "#FFFFFF",
+      red0 = "#8f3d3d",
+      red1 = "#8f443d",
+      red2 = "#8f4b3d",
+      red3 = "#8f523d",
+      red4 = "#8f583d",
+      red5 = "#8f5f3d",
+
+      orange0 = "#8f663d",
+      orange1 = "#8f6d3d",
+      orange2 = "#8f743d",
+      orange3 = "#8f7a3d",
+      orange4 = "#8f813d",
+      orange5 = "#8f883d",
+
+      yellow0 = "#8f8f3d",
+      yellow1 = "#888f3d",
+      yellow2 = "#818f3d",
+      yellow3 = "#7a8f3d",
+      yellow4 = "#748f3d",
+      yellow5 = "#6d8f3d",
+
+      yellow_green0 = "#668f3d",
+      yellow_green1 = "#5f8f3d",
+      yellow_green2 = "#588f3d",
+      yellow_green3 = "#528f3d",
+      yellow_green4 = "#4b8f3d",
+      yellow_green5 = "#448f3d",
+
+      green0 = "#3d8f3d",
+      green1 = "#3d8f44",
+      green2 = "#3d8f4b",
+      green3 = "#3d8f52",
+      green4 = "#3d8f58",
+      green5 = "#3d8f5f",
+
+      turquoise0 = "#3d8f66",
+      turquoise1 = "#3d8f6d",
+      turquoise2 = "#3d8f74",
+      turquoise3 = "#3d8f7a",
+      turquoise4 = "#3d8f81",
+      turquoise5 = "#3d8f88",
+
+      cyan_blue0 = "#3d8f8f",
+      cyan_blue1 = "#3d888f",
+      cyan_blue2 = "#3d818f",
+      cyan_blue3 = "#3d7a8f",
+      cyan_blue4 = "#3d748f",
+      cyan_blue5 = "#3d6d8f",
+
+      indigo0 = "#3d668f",
+      indigo1 = "#3d5f8f",
+      indigo2 = "#3d588f",
+      indigo3 = "#3d528f",
+      indigo4 = "#3d4b8f",
+      indigo5 = "#3d448f",
+
+      blue0 = "#3d3d8f",
+      blue1 = "#443d8f",
+      blue2 = "#4b3d8f",
+      blue3 = "#523d8f",
+      blue4 = "#583d8f",
+      blue5 = "#5f3d8f",
+
+      purple0 = "#663d8f",
+      purple1 = "#6d3d8f",
+      purple2 = "#743d8f",
+      purple3 = "#7a3d8f",
+      purple4 = "#813d8f",
+      purple5 = "#883d8f",
+
+      magenta0 = "#8f3d8f",
+      magenta1 = "#8f3d88",
+      magenta2 = "#8f3d81",
+      magenta3 = "#8f3d7a",
+      magenta4 = "#8f3d74",
+      magenta5 = "#8f3d6d",
+
+      peach0 = "#8f3d66",
+      peach1 = "#8f3d5f",
+      peach2 = "#8f3d58",
+      peach3 = "#8f3d52",
+      peach4 = "#8f3d4b",
+      peach5 = "#8f3d44",
+
+      bg1 = "#262A36",
+      bg2 = "#191919",
+      fg1 = "#969aa3",
     },
     light = {
       black1 = "#c0caf5",
@@ -35,59 +101,54 @@ end
 
 local function get_groups()
   local colors = get_colors()
-
   local groups = {
-    -- 普通文本
-    Normal = { fg = "#969aa3", bg = "#0D0B0B" },
-    --- 浮动窗口的普通文本
-    NormalFloat = { link = "Normal" },
-    -- 非当前窗口的普通文本
-    NormalNC = { link = "Normal" },
-    -- More Message
-    MoreMsg = { link = "Normal" },
-    -- Mode Message
-    ModeMsg = { link = "Normal" },
-    -- 行号
-    LineNr = { fg = "#404040", bg = "#0D0B0B" },
-    -- 搜索结果
-    Search = { reverse = true },
+    CustomVisual = { bg = "#262626", reverse = false },
+    Normal = { fg = colors.fg1, bg = colors.bg1 }, -- 普通文本
+    NormalFloat = { link = "Normal" }, --- 浮动窗口的普通文本
+    NormalNC = { link = "Normal" }, -- 非当前窗口的普通文本
+    MoreMsg = { link = "Normal" }, -- More Message
+    ModeMsg = { link = "Normal" }, -- Mode Message
+    LineNr = { fg = "#404040", bg = colors.bg1 }, -- 行号
+    Search = { reverse = true }, -- 搜索结果
     IncSearch = { link = "Search" },
     CurSearch = { link = "IncSearch" },
-    -- 状态栏
-    StatusLine = { bg = "#0D0B0B", fg = "#969aa3", reverse = true },
-    -- 符号列
-    SignColumn = { bg = "#0D0B0B" },
-    -- 光标行
-    CursorLine = { bg = "#0D0B0B" },
+    StatusLine = { bg = colors.bg1, fg = "#404040", reverse = true }, -- 状态栏
+    SignColumn = { bg = colors.bg1 }, -- 符号列
+    CursorLine = { bg = colors.bg2 }, -- 光标行
     CursorLineNr = { fg = colors.black10 },
     ColorColumn = { bg = colors.black2 },
-    Visual = { bg = "#24272F", reverse = false },
+    Visual = { link = "CustomVisual" },
     VisualNOS = { link = "Visual" },
-    -- 标签栏
-    TabLineFill = { bg = "#0C0E11" },
-    -- 非当前标签
-    TabLine = { link = "TabLineFill" },
-    -- 当前标签选中
-    TabLineSel = { fg = "#36BBAC", bg = "#191919", bold = true },
+    TabLineFill = { bg = "#0C0E11" }, -- 标签栏
+    TabLine = { link = "TabLineFill" }, -- 非当前标签
+    TabLineSel = { fg = "#36BBAC", bg = colors.bg2, bold = true }, -- 当前标签选中
     WarningMsg = { fg = "#F2B33F" },
     Error = { fg = "#C54659" },
-    ErrorMsg = { fg = "#0D0B0B", bg = "#C54659", bold = true },
+    ErrorMsg = { fg = colors.bg1, bg = "#C54659", bold = true },
     MatchParen = { reverse = true },
     -- Conceal = { fg = colors.blue },
-    Keyword = { fg = "#c07c7c", bold = true },
+    Keyword = { fg = "#937cc0", bold = true },
+    Function = { fg = "#7c8dc0", bold = true },
+    String = { fg = "#a9c07c" },
+    Number = { bg = colors.bg1, fg = "#bf4055" },
+    Operator = { fg = colors.fg1 },
+    Delimiter = { fg = colors.fg1 },
+    Comment = { fg = "#4b4d52" },
+    Boolean = { fg = "#bf5540", bold = true },
+    Type = { fg = colors.turquoise5, bold = true },
+    Constant = { fg = colors.cyan_blue0, bold = true },
+    Directory = { fg = "#6a40bf", bold = true },
 
-    ["@variable"] = { fg = "#7C9DC0" },
+    -- Underlined = { fg = colors.blue, underline = config.underline }, -- 突出的文本，HTML链接
+
+    ["@variable"] = { fg = colors.fg1 },
     ["@variable.parameter"] = { fg = "#7C9DC0" },
-    ["@variable.builtin"] = { fg = "#59B38C", bold = true },
-    ["@variable.member"] = { fg = "#969aa3" },
-    ["@lsp.type.variable"] = {},
-
+    ["@variable.builtin"] = { fg = colors.turquoise1, bold = true },
+    ["@variable.member"] = { fg = "#937cc0" },
     ["@module.builtin"] = { link = "@variable.builtin" },
-    -- ["@constant.builtin"] = { link = "@variable.builtin" },
     ["@property"] = {},
-    ["@lsp.type.property"] = { link = "@property" },
     ["@keyword"] = { link = "Keyword" },
-    ["@keyword.function"] = { fg = "#c07c7c", bold = true },
+    ["@keyword.function"] = { fg = "#937cc0", bold = true },
     ["@keyword.return"] = { fg = "#c07c7c", bold = true },
     -- ["@keyword.conditional"] = { bg = "#191C20", fg = "#8A7B7A", bold = true },
     -- ["@keyword.repeat"] = { bg = "#191C20", fg = "#8A8A9A", bold = true },
@@ -96,23 +157,13 @@ local function get_groups()
     -- ["@keyword.directive.define"] = { link = "Define" },
     -- ["@keyword.exception"] = { link = "Exception" },
     -- ["@keyword.import"] = { link = "Include" },
-    -- ["@keyword.operator"] = { link = "GruvboxRed" },
+    -- ["@keyword.operator"] = { link = "Gruvboxpeach4" },
     -- ["@keyword.storage"] = { link = "StorageClass" },
-
-    Function = { fg = "#87c07c", bold = true },
-    String = { fg = "#c0c07c" },
-    Number = { bg = "#0D0B0B", fg = "#C54659" },
-    Operator = { fg = "#969aa3" },
-    Delimiter = { fg = "#969aa3" },
-    Comment = { fg = "#4b4d52" },
-    Boolean = { bg = "#0D0B0B", fg = "#DA8D59", bold = true },
-    Type = { fg = "#59B38C", bold = true },
     ["@function"] = { link = "Function" },
     ["@function.call"] = { link = "Function" },
     ["@function.builtin"] = { link = "Function" },
     -- ["@function.macro"] = { link = "Macro" },
     -- ["@function.method"] = { link = "Function" },
-    ["@string"] = { link = "String" },
     ["@operator"] = { link = "Operator" },
     ["@punctuation.delimiter"] = { link = "Delimiter" },
     ["@punctuation.bracket"] = { link = "Delimiter" },
@@ -120,46 +171,136 @@ local function get_groups()
     ["@constructor"] = { link = "Delimiter" },
     ["@comment"] = { fg = "#4b4d52" },
     ["@type"] = { link = "Type" },
-    ["@lsp.type.class"] = { link = "@type" },
+    ["@type.builtin"] = { link = "Type" },
     ["@number"] = { link = "Number" },
     ["@number.float"] = { link = "Float" },
     ["@boolean"] = { link = "Boolean" },
     ["@constant"] = { link = "Constant" },
-    -- BlinkCmpLabelDeprecated = { link = "GruvboxFg1" },
-    BlinkCmpKind = { fg = "#C54659" },
-    -- BlinkCmpLabel = { bg = "#C54659" },
-    BlinkCmpLabelMatch = { fg = "#8F5FDE" },
+    ["@constant.builtin"] = { link = "Constant" },
+    ["@tag"] = { fg = colors.indigo5, bold = true },
+    ["@tag.attribute"] = { fg = "#7c7cc0" },
+    ["@tag.delimiter"] = { link = "Delimiter" },
+    ["@tag.builtin"] = { fg = colors.turquoise3, bold = false },
+
+    ["@lsp.type.variable"] = {},
+    ["@lsp.type.property"] = { link = "@property" },
+    ["@lsp.type.class"] = { link = "@type" },
+    -- ["@lsp.type.comment"] = { link = "@comment" },
+    -- ["@lsp.type.decorator"] = { link = "@macro" },
+    -- ["@lsp.type.enum"] = { link = "@type" },
+    -- ["@lsp.type.enumMember"] = { link = "@constant" },
+    -- ["@lsp.type.function"] = { link = "@function" },
+    -- ["@lsp.type.interface"] = { link = "@constructor" },
+    -- ["@lsp.type.macro"] = { link = "@macro" },
+    -- ["@lsp.type.method"] = { link = "@method" },
+    -- ["@lsp.type.modifier.java"] = { link = "@keyword.type.java" },
+    -- ["@lsp.type.namespace"] = { link = "@namespace" },
+    -- ["@lsp.type.parameter"] = { link = "@parameter" },
+    -- ["@lsp.type.struct"] = { link = "@type" },
+    -- ["@lsp.type.type"] = { link = "@type" },
+    -- ["@lsp.type.typeParameter"] = { link = "@type.definition" },
+    -- ["@text"] = { link = "GruvboxFg1" },
+    -- ["@text.strong"] = { bold = config.bold },
+    -- ["@text.emphasis"] = { italic = config.italic.emphasis },
+    -- ["@text.underline"] = { underline = config.underline },
+    -- ["@text.strike"] = { strikethrough = config.strikethrough },
+    -- ["@text.title"] = { link = "Title" },
+    -- ["@text.literal"] = { link = "String" },
+    -- ["@text.uri"] = { link = "Underlined" },
+    -- ["@text.math"] = { link = "Special" },
+    -- ["@text.environment"] = { link = "Macro" },
+    -- ["@text.environment.name"] = { link = "Type" },
+    -- ["@text.reference"] = { link = "Constant" },
+    -- ["@text.todo"] = { link = "Todo" },
+    -- ["@text.todo.checked"] = { link = "GruvboxGreen" },
+    -- ["@text.todo.unchecked"] = { link = "GruvboxGray" },
+    -- ["@text.note"] = { link = "SpecialComment" },
+    -- ["@text.note.comment"] = { fg = colors.purple, bold = config.bold },
+    -- ["@text.warning"] = { link = "WarningMsg" },
+    -- ["@text.danger"] = { link = "ErrorMsg" },
+    -- ["@text.danger.comment"] = { fg = colors.fg0, bg = colors.red, bold = config.bold },
+    -- ["@text.diff.add"] = { link = "diffAdded" },
+    -- ["@text.diff.delete"] = { link = "diffRemoved" },
+
+    ["@string"] = { link = "String" },
+    ["@string.escape"] = { fg = colors.peach4 },
+    -- ["@string.regex"] = { link = "String" },
+    -- ["@string.regexp"] = { link = "String" },
+    -- ["@string.special"] = { link = "SpecialChar" },
+    -- ["@string.special.path"] = { link = "Underlined" },
+    -- ["@string.special.symbol"] = { link = "Identifier" },
+    -- ["@string.special.url"] = { link = "Underlined" },
+    ["@none"] = { bg = "NONE", fg = "NONE" },
+
+    DiagnosticUnderlineWarn = { underline = true, sp = colors.orange0 },
+    -- BlinkCmpLabelDeprecated = { bg = "black" },
+    BlinkCmpKind = { fg = colors.peach4 },
+    -- BlinkCmpLabel = { bg = "black" },
+    BlinkCmpLabelMatch = { fg = colors.turquoise5 },
     BlinkCmpMenuSelection = { bg = "#262626" },
-    BlinkCmpMenu = { bg = "#191919" },
-    BlinkCmpSource = { fg = "#7C9DC0" },
+    BlinkCmpMenu = { bg = colors.bg2 },
+    BlinkCmpSource = { fg = "#404040" },
     -- BlinkCmpLabelDetail = { link = "GruvboxGray" },
     -- BlinkCmpLabelDescription = { link = "GruvboxGray" },
-    -- BlinkCmpKindText = { link = "GruvboxOrange" },
+    BlinkCmpKindText = { fg = colors.yellow_green0 },
     -- BlinkCmpKindVariable = { link = "GruvboxOrange" },
-    -- BlinkCmpKindMethod = { link = "GruvboxBlue" },
-    -- BlinkCmpKindFunction = { link = "GruvboxBlue" },
-    -- BlinkCmpKindConstructor = { link = "GruvboxYellow" },
+    BlinkCmpKindMethod = { link = "BlinkCmpKindFunction" },
+    BlinkCmpKindFunction = { fg = colors.purple1 },
+    -- BlinkCmpKindConstructor = { link = "Gruvboxorange2" },
     -- BlinkCmpKindUnit = { link = "GruvboxBlue" },
     -- BlinkCmpKindField = { link = "GruvboxBlue" },
-    -- BlinkCmpKindClass = { link = "GruvboxYellow" },
-    -- BlinkCmpKindInterface = { link = "GruvboxYellow" },
+    -- BlinkCmpKindClass = { link = "Gruvboxorange2" },
+    -- BlinkCmpKindInterface = { link = "Gruvboxorange2" },
     -- BlinkCmpKindModule = { link = "GruvboxBlue" },
     -- BlinkCmpKindProperty = { link = "GruvboxBlue" },
     -- BlinkCmpKindValue = { link = "GruvboxOrange" },
-    -- BlinkCmpKindEnum = { link = "GruvboxYellow" },
-    -- BlinkCmpKindOperator = { link = "GruvboxYellow" },
-    -- BlinkCmpKindKeyword = { link = "GruvboxPurple" },
-    -- BlinkCmpKindEvent = { link = "GruvboxPurple" },
-    -- BlinkCmpKindReference = { link = "GruvboxPurple" },
-    -- BlinkCmpKindColor = { link = "GruvboxPurple" },
-    -- BlinkCmpKindSnippet = { link = "GruvboxGreen" },
+    -- BlinkCmpKindEnum = { link = "Gruvboxorange2" },
+    -- BlinkCmpKindOperator = { link = "Gruvboxorange2" },
+    -- BlinkCmpKindKeyword = { link = "Gruvboxblue4" },
+    -- BlinkCmpKindEvent = { link = "Gruvboxblue4" },
+    -- BlinkCmpKindReference = { link = "Gruvboxblue4" },
+    -- BlinkCmpKindColor = { link = "Gruvboxblue4" },
+    -- BlinkCmpKindSnippet = { link = "Gruvboxgreen4" },
     -- BlinkCmpKindFile = { link = "GruvboxBlue" },
     -- BlinkCmpKindFolder = { link = "GruvboxBlue" },
     -- BlinkCmpKindEnumMember = { link = "GruvboxAqua" },
     -- BlinkCmpKindConstant = { link = "GruvboxOrange" },
-    -- BlinkCmpKindStruct = { link = "GruvboxYellow" },
-    -- BlinkCmpKindTypeParameter = { link = "GruvboxYellow" },
+    -- BlinkCmpKindStruct = { link = "Gruvboxorange2" },
+    -- BlinkCmpKindTypeParameter = { link = "Gruvboxorange2" },
     -- BlinkCmpGhostText = { link = "GruvboxBg4" },
+
+    NeoTreeDirectoryName = { link = "Directory" },
+    NeoTreeGitUntracked = { fg = colors.orange0, bold = true },
+    NeoTreeGitModified = { fg = colors.cyan_blue0, bold = true },
+    -- NeoTreeDotfile = { fg = colors.fg4 },
+    -- NeoTreeFadeText1 = { fg = colors.fg3 },
+    -- NeoTreeFadeText2 = { fg = colors.fg4 },
+    -- NeoTreeFileIcon = { fg = colors.blue },
+    -- NeoTreeFileName = { fg = colors.fg1 },
+    -- NeoTreeFileNameOpened = { fg = colors.fg1, bold = true },
+    -- NeoTreeFileStats = { fg = colors.fg3 },
+    -- NeoTreeFileStatsHeader = { fg = colors.fg2, italic = true },
+    -- NeoTreeFilterTerm = { link = "SpecialChar" },
+    -- NeoTreeHiddenByName = { link = "NeoTreeDotfile" },
+    -- NeoTreeIndentMarker = { fg = colors.fg4 },
+    -- NeoTreeMessage = { fg = colors.fg3, italic = true },
+    -- NeoTreeModified = { fg = colors.yellow },
+    -- NeoTreeRootName = { fg = colors.fg1, bold = true, italic = true },
+    -- NeoTreeSymbolicLinkTarget = { link = "NeoTreeFileName" },
+    -- NeoTreeExpander = { fg = colors.fg4 },
+    -- NeoTreeWindowsHidden = { link = "NeoTreeDotfile" },
+    -- NeoTreePreview = { link = "Search" },
+    -- NeoTreeGitAdded = { link = "GitGutterAdd" },
+    -- NeoTreeGitConflict = { fg = colors.orange, bold = true, italic = true },
+    -- NeoTreeGitDeleted = { link = "GitGutterDelete" },
+    -- NeoTreeGitIgnored = { link = "NeoTreeDotfile" },
+    -- NeoTreeGitRenamed = { link = "NeoTreeGitModified" },
+    -- NeoTreeGitStaged = { link = "NeoTreeGitAdded" },
+    -- NeoTreeGitUnstaged = { link = "NeoTreeGitConflict" },
+    -- NeoTreeTabActive = { fg = colors.fg1, bold = true },
+    -- NeoTreeTabInactive = { fg = colors.fg4, bg = colors.bg1 },
+    -- NeoTreeTabSeparatorActive = { fg = colors.bg1 },
+    -- NeoTreeTabSeparatorInactive = { fg = colors.bg2, bg = colors.bg1 },
   }
 
   return groups
@@ -178,6 +319,21 @@ function M.loader()
 end
 
 return M
+
+-- 40 40
+-- 350
+-- #8f3d4b
+-- 260
+-- #583d8f
+-- 40
+-- #8f743d
+-- 10
+-- #8f4b3d
+--
+-- 120
+-- #3d8f3d
+-- 160
+-- #3d8f74
 
 -- 卡
 -- #BE5454
@@ -276,3 +432,27 @@ return M
 -- #7CC0C0 - 青色
 -- #7CC0B0 - 青绿色
 -- #7CC0A0 - 绿色
+-- black0 = "#000000",
+-- black1 = "#0d0d0d",
+-- black2 = "#191919",
+-- black3 = "#262626",
+-- black4 = "#333333",
+-- black5 = "#404040",
+-- black6 = "#4d4d4d",
+-- black7 = "#595959",
+-- black8 = "#666666",
+-- black9 = "#737373",
+-- black10 = "#808080",
+-- black11 = "#8C8C8C",
+-- black12 = "#999999",
+-- black13 = "#A6A6A6",
+-- black14 = "#B3B3B3",
+-- black15 = "#BFBFBF",
+-- black16 = "#CCCCCC",
+-- black17 = "#D9D9D9",
+-- black18 = "#E6E6E6",
+-- black19 = "#F2F2F2",
+-- black20 = "#FFFFFF",
+-- #222831
+-- #1D1616
+-- #0C0C0C
--- lua/custom/plugins/magic/lua/magic/key/init.lua
+++ lua/custom/plugins/magic/lua/magic/key/init.lua
@@ -2,7 +2,7 @@ vim.api.nvim_set_hl(0, "CustomMagicVisual", {
   fg = "#cdcdcd",
   bg = "#333738",
 })
-local visual_hl = vim.api.nvim_get_hl(0, { name = "Visual" })
+local visual_hl = vim.api.nvim_get_hl(0, { name = "CustomVisual" })
 vim.api.nvim_set_hl(0, "CustomMagicNextKey", {
   fg = "#ff007c",
 })
